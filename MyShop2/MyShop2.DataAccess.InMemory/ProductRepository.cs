2f6fb9ff7f49016879459ba225a857baf26195ed
ehtisham80 <ehtisham_zaheer@hotmail.com>
2020-08-07 19:04:37 +0200

    Product Model and InMemory Product Repository Build

---
+++ MyShop2/MyShop2.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop2.Core.Models;
+
+namespace MyShop2.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+    }
+}
